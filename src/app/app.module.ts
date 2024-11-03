0085062d1cd18168fbac351e3502a17d300b9650
SeanPalomares <u202121947@upc.edu.pe>
2024-11-03 17:07:53 -0500

    Se añadieron elementos base para los componentes, modelos y páginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarWriterComponent } from './shared/components/navbar-writer/navbar-writer.component';
+import { NavbarReaderComponent } from './shared/components/navbar-reader/navbar-reader.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
+import { CategoryFormComponent } from './pages/writer/category-form/category-form.component';
+import { CategoryListComponent } from './pages/writer/category-list/category-list.component';
+import { ArticleListComponent } from './pages/writer/article-list/article-list.component';
+import { ArticleFormComponent } from './pages/writer/article-form/article-form.component';
+import { WriterLayoutComponent } from './pages/writer/writer-layout/writer-layout.component';
+import { AuthLayoutComponent } from './pages/auth/auth-layout/auth-layout.component';
+import { ArticleCatalogComponent } from './pages/article/article-catalog/article-catalog.component';
+import { ArticleDetailComponent } from './pages/article/article-detail/article-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarWriterComponent,
+    NavbarReaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    CategoryFormComponent,
+    CategoryListComponent,
+    ArticleListComponent,
+    ArticleFormComponent,
+    WriterLayoutComponent,
+    AuthLayoutComponent,
+    ArticleCatalogComponent,
+    ArticleDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/components/navbar-reader/navbar-reader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-reader',
+  templateUrl: './navbar-reader.component.html',
+  styleUrl: './navbar-reader.component.css'
+})
+export class NavbarReaderComponent {
+
+}
