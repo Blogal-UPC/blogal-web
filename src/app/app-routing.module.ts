c309d1aac3622008740bc1fdbdec8c88c0074e63
rchui17 <103223281+rchui17@users.noreply.github.com>
2024-11-07 18:06:01 -0500

    Feat: Foro de preguntas para los creadores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EarningsComponent } from './pages/earnings/earnings.component';
 import { PublicationComponent } from './publication/publication.component';
+import { QuestionsComponent } from './pages/questions/questions.component';
 
 const routes: Routes = [{path:'earnings', component:EarningsComponent},
-  {path:'publication', component:PublicationComponent}
+  {path:'publication', component:PublicationComponent},
+  {path:'questions', component:QuestionsComponent}
 ];
 
 @NgModule({
