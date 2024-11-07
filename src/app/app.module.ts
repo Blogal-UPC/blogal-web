fcbba2ba5345e6fc8e962ba69bb809a19d83c235
rchui17 <103223281+rchui17@users.noreply.github.com>
2024-11-07 17:56:34 -0500

    Feat: Branch de preguntas al creador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { ArticleCatalogComponent } from './pages/article/article-catalog/article
 import { ArticleDetailComponent } from './pages/article/article-detail/article-detail.component';
 import { PublicationComponent } from './publication/publication.component';
 import { EarningsComponent } from './pages/earnings/earnings.component';
+import { QuestionsComponent } from './pages/questions/questions.component';
 
 @NgModule({
   declarations: [
@@ -42,7 +43,8 @@ import { EarningsComponent } from './pages/earnings/earnings.component';
     ArticleCatalogComponent,
     ArticleDetailComponent,
     PublicationComponent,
-    EarningsComponent
+    EarningsComponent,
+    QuestionsComponent
   ],
   imports: [
     BrowserModule,
